5fb6f11b94dfbe27ee46c850c5c16c7f678f50cb
pupil <pupil@ivkhk.ee>
2022-03-18 11:05:18 +0200

    Добавил файл с функциями, с добавлением и выводом клиентов и продуктов.

---
+++ functions.py
@@ -0,0 +1,75 @@
+from sqlite3 import Cursor
+import mysql.connector
+
+conn = mysql.connector.connect(host="localhost", port="3306", user="pyshop", password="pyshop", database="pyshop")
+cursor = conn.cursor()
+
+
+'''ДОБАВЛЕНИЕ КЛИЕНТА'''
+
+def addClient(firstName, lastName, phone, money):
+    try:
+        cursor.execute('CREATE TABLE tClient(clientID int AUTO_INCREMENT primary key, firstName varchar(25) not null, lastName varchar(25) not null, phone varchar(8) not null, money float not null)')
+        conn.commit()
+    except:
+        print("")
+    try:
+        cursor.execute("INSERT INTO tClient VALUES (null, %s, %s, %s, %s)", (firstName, lastName, phone, money))
+        conn.commit()
+        print("Пользователь", firstName, lastName, "добавлен в базу")
+        print("=======================================")
+    except:
+        print("Авария! Нет электричества! Ждём!")
+        print("==================================")
+
+
+'''ДОБАВЛЕНИЕ ПРОДУКТА'''
+
+def addProduct(productName, productWeight, productPrice):
+    try:
+        cursor.execute('CREATE TABLE tProduct(productID int AUTO_INCREMENT primary key, productName varchar(25) not null, productWeight float not null, productPrice float not null)')
+        conn.commit()
+    except:
+        print("")
+    try:
+        cursor.execute("INSERT INTO tProduct VALUES (null, %s, %s, %s)", (productName, productWeight, productPrice))
+        conn.commit()
+        print(productName, "добавлен в магазин")
+        print("=======================================")
+    except:
+        print("Авария! Нет электричества! Ждём!")
+
+
+'''СПИСОК КЛИЕНТОВ'''
+
+def listClients():
+    print("Список клиентов: ")
+    try:
+        cursor.execute('SELECT * FROM tClient')
+        record = cursor.fetchall()
+        print("==================")
+        for row in record:
+            print("Имя: ", row[1])
+            print("Фамилия: ", row[2])
+            print("Телефон: ", row[3])
+            print("Кошелек: ", row[4], "$")
+            print("==================")
+    except:
+        print("В наличии нет ни одного клиента!")
+
+
+'''СПИСОК ПРОДУКТОВ'''
+
+def listProducts():
+    print("Список продуктов: ")
+    try:
+        cursor.execute('SELECT * FROM tProduct')
+        record = cursor.fetchall()
+        print("==================")
+        for row in record:
+            print("Название: ", row[1])
+            print("Вес: ", row[2], "г")
+            print("Цена: ", row[3], "$")
+            print("==================")
+    except:
+        print("В наличии нет ни одного продукта!")
--- shop.py
+++ shop.py
@@ -1,6 +1,6 @@
-import array as arr
 from sqlite3 import Cursor
 import mysql.connector
+from functions import addClient, addProduct, listClients, listProducts, Buy
 
 
 conn = mysql.connector.connect(host="localhost", port="3306", user="pyshop", password="pyshop", database="pyshop")
@@ -23,7 +23,7 @@ while operation != 7:
     if choose == 1:
         print("Введите имя: ")
         firstName = input()
-        
+       
         print("Введите фамилию: ")
         lastName = input()
         
@@ -33,36 +33,10 @@ while operation != 7:
         print("Введите бабки: ")
         money = float(input())
 
-        # with open(clientFile, "a") as file:
-        #     file.write(firstName + "\n" + lastName + "\n" + str(phone) + "\n" + str(money) + "\n")
-
-        try:
-            cursor.execute('CREATE TABLE tClient(clientID int identity primary key not null, firstName varchar(25) not null, lastName varchar(25) not null, phone varchar(8) not null, money float not null)')
-            conn.commit()
-        except:
-            print("")
-        try:
-            cursor.execute("INSERT INTO tClient VALUES (%s, %s, %s, %s)", (firstName, lastName, phone, money))
-            conn.commit()
-            print("Пользователь", firstName, lastName, "добавлен в базу")
-            print("=======================================")
-        except:
-            print("Авария! Нет электричества! Ждём!")
+        addClient(firstName, lastName, phone, money)
             
     elif choose == 2:
-        print("Список клиентов: ")
-        try:
-            cursor.execute('SELECT * FROM tClient')
-            record = cursor.fetchall()
-            print("==================")
-            for row in record:
-                print("Имя: ", row[0])
-                print("Фамилия: ", row[1])
-                print("Телефон: ", row[2])
-                print("Кошелек: ", row[3], "$")
-                print("==================")
-        except:
-            print("В базе нет ни одного клиента!")
+        listClients()
           
     elif choose == 3:
         print("Введите название продукта: ")
@@ -74,38 +48,19 @@ while operation != 7:
         print("Введите цену: ")
         productPrice = float(input())
         
-        try:
-            cursor.execute('CREATE TABLE tProduct(productName varchar(25) not null, productWeight float not null, productPrice float not null)')
-            conn.commit()
-        except:
-            print("")
-        try:
-            cursor.execute("INSERT INTO tProduct VALUES (%s, %s, %s)", (productName, productWeight, productPrice))
-            conn.commit()
-            print(productName, "добавлен в базу")
-            print("=======================================")
-        except:
-            print("Авария! Нет электричества! Ждём!")
+        addProduct(productName, productWeight, productPrice)
     
     elif choose == 4:
-        print("Список продуктов: ")
-        try:
-            cursor.execute('SELECT * FROM tProduct')
-            record = cursor.fetchall()
-            print("==================")
-            for row in record:
-                print("Название: ", row[0])
-                print("Вес: ", row[1], "г")
-                print("Цена: ", row[2], "$")
-                print("==================")
-        except:
-            print("В базе нет ни одного продукта!")
+        listProducts()
 
     elif choose == 5:
         print("Добавить бабки: ")
         addMoney = int(input())
         sumMoney = addMoney + money
         print(sumMoney)
+
+    elif choose == 6:
+        Buy()
 
     elif choose == 7:
         print("Вы вышли")
